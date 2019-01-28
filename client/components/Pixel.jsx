4d03f2d8ca96d5a8a09a55364d3b84191db6f471
fetuolemoana <fetu.tamapeau@gmail.com>
2019-01-29 10:55:57 +1300

    Let there be colour task complete

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,4 +1,16 @@
 import React from 'react'
 
+class Pixel extends React.Component {
+  render () {
+    return (
+      <div style={{
+        fontFamily: 'Times New Roman',
+        height: '5px',
+        width: '5px',
+        backgroundColor: 'teal'
+      }}></div>
+    )
+  }
+}
 
 export default Pixel
