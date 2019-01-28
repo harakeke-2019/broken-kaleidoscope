e1f737ab229fb14e1ca263ab98dc69564a7d5409
fetuolemoana <fetu.tamapeau@gmail.com>
2019-01-29 11:53:56 +1300

    State your business complete

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
 class Pixel extends React.Component {
+  constructor (props) {
+    super(props)
+      this.state = {
+        style: {
+          fontFamily: 'Times New Roman',
+          height: '20px',
+          width: '20px',
+          backgroundColor: 'teal'
+        }
+      }
+  }
   render () {
     return (
-      <div style={{
-        fontFamily: 'Times New Roman',
-        height: '5px',
-        width: '5px',
-        backgroundColor: 'teal'
-      }}></div>
+      <div style = {this.state.style}></div>
     )
   }
 }
