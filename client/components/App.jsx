8b67febca821b95968e3eced4984cffe633aa73e
Lane le Prevost-Smith <lanelps@gmail.com>
2019-01-29 10:53:01 +1300

    added Pixel component into App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+
+import Pixel from "./Pixel";
 
 const App = () => {
   return (
-    <div>Yup.</div>
-  )
-}
+    <div>
+      <Pixel />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pixel = () => {
+  return <div />;
+};
+
+export default Pixel;
