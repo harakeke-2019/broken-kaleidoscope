0261fdb81bcc04659c48b08707dc0d6cf3a0e1d9
Jason James Simpson <jasonjamessimpson@gmail.com>
2019-01-30 10:27:54 +1300

    added onclick

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,42 +1,35 @@
-import React, { Component} from 'react'
+import React, {Component} from 'react'
 
 const randomHexColor = () =>
   `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
 class Pixel extends Component {
-  constructor (props){
+  constructor (props) {
     super(props)
-    this.state={
-        style:{
-              height: '100px',
-              width: '100px',
-              backgroundColor: randomHexColor ()
-            }
-        }
+    this.state = {
+      style: {
+        height: '100px',
+        width: '100px',
+        backgroundColor: randomHexColor()
+      }
     }
+  }
 
+  setStyle = () => {
+    this.setState({
+      style: {this.state.style}
+    })
+  }
 
-
-
-// setStyle = () => {
-//   this.setState({
-//     style: {this.state.style}
-//   })
-// }
-
-
-render(){
-  return(
-    <div style={this.state.style}>
-    </div>
-  )
+  render () {
+    return (
+      <div onClick={setStyle} style={this.state.style}>
+      </div>
+    )
+  }
 }
-}
-
-
 
 export default Pixel
-
 
 // style: {
 //       height: {height: '100px'},
