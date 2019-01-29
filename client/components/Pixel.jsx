d08b6534ba562bad82ad9fbb3105921a846a920f
Dawid Jacobus Kotze Luttig <dluttig5@live.com>
2019-01-29 14:04:37 +1300

    Created Rainbow pixels

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 
-const Pixel = () => {
-  return (
-    <div style= {{
-      height: '20px',
-      width: '20px',
-      backgroundColor: 'blue'
-    }}></div>
-  )
+// const Pixel = () => {
+//   return (
+//     <div style= {{
+//       height: '40px',
+//       width: '40px',
+//       backgroundColor: 'blue'
+//     }}></div>
+//   )
+// }
+const randomHexColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+class Pixel extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      style: {
+        height: '40px',
+        width: '40px',
+        backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+      }
+    }
+  }
+  render () {
+    return (
+      <div style = {this.state.style} ></div>
+    )
+  }
 }
 
 export default Pixel
