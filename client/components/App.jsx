af12fb0cb6411a289c4e61a96af0a758fae6b614
aubreytekanawa <aubreytekanawa@gmail.com>
2019-01-30 15:36:28 +1300

    initialise repo

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Pix from './Pix'
 
 const App = () => {
+  array.map()
+  let array = []
+  for (let i = 0; i < 100; i++)
+  array.push(<Pixel key={i}/>)
   return (
-    <div>Yup.</div>
+    <div>
+      {array}
+    </div>
   )
 }
 
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const randomColour = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`
+const width = '5%'
+const height = '10%'
+
+export default class Pix extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      style: {
+        width,
+        height,
+        backgroundColor: randomColour()
+      }
+    }
+  }
+
+  changeColour = () => {
+    const style = {
+      width,
+      height,
+      backgroundColor: randomColour()
+    }
+
+    this.setState({
+      style
+    })
+  }
+
+  render () {
+    const { style } = this.state
+    return (
+      <div
+        onClick={() => this.changeColour()}
+        onMouseEnter={() => this.changeColour()}
+        style={style} />
+    )
+  }
+}
