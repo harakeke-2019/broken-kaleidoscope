129545b8b8dc5129c3c1e3ed2ac859326ce3bdc0
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-02-04 15:05:39 +1300

    mvp done

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -9,8 +9,8 @@ const randomHexColor = () =>
         super()
         this.state = {
             style: {
-                width: "10px",
-                height: "10px",
+                width: "20px",
+                height: "20px",
                 backgroundColor: randomHexColor()
             }
         }
@@ -24,10 +24,49 @@ const randomHexColor = () =>
                 backgroundColor: "black"
         }})
     }
+
+    mouseEnter = ()=>{
+        this.setState ({
+            style:{
+                width:"20px",
+                height: "20px",
+                backgroundColor: "green"
+            }})
+    }
+
+    contextMenu = ()=>{
+        event.preventDefault()
+        this.setState ({
+            style:{
+                width:"20px",
+                height: "20px",
+                backgroundColor: "aqua"
+            }})
+    }
+
+    doubleClick = ()=>{
+        this.setState ({
+            style:{
+                width:"20px",
+                height: "20px",
+                backgroundColor: "white"
+            }})
+    }
+
+    dragEnter = ()=>{
+        this.setState ({
+            style:{
+                width:"20px",
+                height: "20px",
+                backgroundColor: "yellow"
+            }})
+    }
+
    
     render(){
         return(
-            <div style = {this.state.style} onClick = {this.clickHandler}></div>
+            <div style = {this.state.style} onClick = {this.clickHandler} onMouseEnter = {this.mouseEnter}
+            onContextMenu = {this.contextMenu} onDoubleClick = {this.doubleClick} onDragEnter ={this.dragEnter}></div>
         )
     }
 }
